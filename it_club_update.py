e1ea5481377b6c30e245cdf3cfed575a4c2dad07
Prajwol Pradhan <57973356+jarp0l@users.noreply.github.com>
2022-10-04 22:00:04 +0545

    Update discord invite link

--- it_club_update.py
+++ it_club_update.py
@@ -17,7 +17,7 @@ README_BASE = """
 
 Email: [itclub@pcampus.edu.np](mailto:itclub@pcampus.edu.np)
 
-Discord: [Join our server](https://discord.gg/YBMhU59P3p)
+Discord: [Join our server](https://discord.gg/DpYZj3pNAv)
 
 Twitter: [@ITClubPulchowk](https://twitter.com/ITClubPulchowk)
 
